b51347bf99c0bab4dd481d5da7121b9973d6f8fb
Andres Pineda Cladera <andrescladera@MacBook-Pro-de-Andres.local>
2021-05-28 22:20:06 +0200

    FiltrosCompletado

--- Entregable2/apijs/matchesLogic.js
+++ Entregable2/apijs/matchesLogic.js
@@ -20,14 +20,14 @@ let radio_empatados = document.getElementById("radio-empatados");
 let radio_perdidos = document.getElementById("radio-perdidos");
 let elementosRadio = document.getElementsByClassName("clase-radio");
 let inputBuscador = document.getElementById("filtro");
+let mensajeError = document.getElementById("error");
 
 let botonPulsado = function () {
     let equipo = inputBuscador.value;
     let partidos = obtenerPartidos(equipo);
     console.log(partidos);
     let partidosObtenidos = partidosFiltradosPorVED(partidos);
-    // limpiarPagina();
-    // pintarPartidosObtenidos(partidosObtenidos);
+    pintarPartidosObtenidos(partidosObtenidos);
 };
 
 
@@ -35,40 +35,58 @@ let obtenerPartidos = function (equipoSeleccionado) {
     partidosTotales.innerHTML = "";
     let partidosDelEquipo = [];
     if (equipoSeleccionado == undefined || equipoSeleccionado == "") {
-        for (let i = 0; i < matchTotales.length; i++) {
-            let result = `${matchTotales[i].score.fullTime.homeTeam} - ${matchTotales[i].score.fullTime.awayTeam}`;
-            let homeT = `${matchTotales[i].homeTeam.name}`;
-            let awayT = `${matchTotales[i].awayTeam.name}`;
-            let tr = document.createElement("tr");
-            tr.innerHTML = [`<td>${homeT}</td>
-        <td>${result}</td>
-        <td>${awayT}</td>`];
-            partidosTotales.appendChild(tr)
-
-        };
+        pintarPartidosObtenidos(matchTotales);
     }
-    for (i = 0; i < matchTotales.length; i++) {
-        if (matchTotales[i].homeTeam.name == equipoSeleccionado || matchTotales[i].awayTeam.name == equipoSeleccionado) {
-            partidosDelEquipo.push(matchTotales[i]);
+    else {
+        for (i = 0; i < matchTotales.length; i++) {
+            if (matchTotales[i].homeTeam.name == equipoSeleccionado || matchTotales[i].awayTeam.name == equipoSeleccionado) {
+                partidosDelEquipo.push(matchTotales[i]);
 
+            }
         }
+        return partidosDelEquipo;
     }
-    return partidosDelEquipo;
+
 };
 
 let partidosFiltradosPorVED = function (partidosSeleccionados) {
+    if (partidosSeleccionados.length == 0){
+        borrarTabla();
+        mensajeError.innerHTML= "No se ha obtenido resultados";
+    }
     let partidosFiltrados = [];
     for (i = 0; i < partidosSeleccionados.length; i++) {
         if (radio_ganados.checked == true && (partidosSeleccionados[i].homeTeam.name == inputBuscador.value && partidosSeleccionados[i].score.winner == "HOME_TEAM" || partidosSeleccionados[i].awayTeam.name == inputBuscador.value && partidosSeleccionados[i].score.winner == "AWAY_TEAM")) {
-            partidosFiltrados.push(partidosSeleccionados[i]);
-        }
+            partidosFiltrados.push(partidosSeleccionados[i])
+
+        } else if (radio_empatados.checked == true && (partidosSeleccionados[i].homeTeam.name == inputBuscador.value && partidosSeleccionados[i].score.winner == "DRAW" || partidosSeleccionados[i].awayTeam.name == inputBuscador.value && partidosSeleccionados[i].score.winner == "DRAW")) {
+            partidosFiltrados.push(partidosSeleccionados[i])
+
+        } else if (radio_perdidos.checked == true && (partidosSeleccionados[i].homeTeam.name == inputBuscador.value && partidosSeleccionados[i].score.winner == "AWAY_TEAM" || partidosSeleccionados[i].awayTeam.name == inputBuscador.value && partidosSeleccionados[i].score.winner == "HOME_TEAM")) {
+            partidosFiltrados.push(partidosSeleccionados[i])
+
+        } else if (radio_ganados.checked == false && radio_perdidos.checked == false && radio_empatados.checked == false) { partidosFiltrados = partidosSeleccionados }
     }
-    console.log(partidosFiltrados);
     return partidosFiltrados;
 };
 
 
+let pintarPartidosObtenidos = function (partidosEquipo) {
+    borrarTabla();
+    for (i = 0; i < partidosEquipo.length; i++) {
+        let tr = document.createElement("tr");
+        tr.innerHTML = [`<td>${partidosEquipo[i].homeTeam.name}</td>
+        <td>${partidosEquipo[i].score.fullTime.homeTeam} - ${partidosEquipo[i].score.fullTime.awayTeam}</td>
+        <td>${partidosEquipo[i].awayTeam.name}</td>`];
+        partidosTotales.appendChild(tr);
+
+    }
+};
+
+let borrarTabla = function () {
+    partidosTotales.innerHTML = "";
+}
 
 
 // llAMADO DE FUNCIONES
-obtenerPartidos()
+obtenerPartidos();
