32c5fae3162ebcb9731bc523772bc900ad77212b
Andres Pineda Cladera <andrescladera@MBP-de-Andres.home>
2021-06-04 21:01:07 +0200

    04/06

--- Entregable2/apijs/matchesLogic.js
+++ Entregable2/apijs/matchesLogic.js
@@ -1,34 +1,56 @@
-let matchTotales = matches.matches;
-
 let partidosTotales = document.getElementById("table-body");
 
 
 let radio_ganados = document.getElementById("radio-ganados");
-let radio_empatados = document.getElementById("radio-empatados");
+let radio_empatados = document.getElementById("radio-empatados").addEventListener("click",console.log("empatados"));
 let radio_perdidos = document.getElementById("radio-perdidos");
 let elementosRadio = document.getElementsByClassName("clase-radio");
 let inputBuscador = document.getElementById("filtro");
 let mensajeError = document.getElementById("error");
 
+let obtenerInformacion = () => {
+    let rankingUrl =
+        "https://api.football-data.org/v2/competitions/2014/matches";
+
+    fetch(rankingUrl, {
+        method: "GET",
+        headers: {
+            "X-Auth-Token": "f4db1fef6dcb4d3494491b461a6c076e",
+        },
+    })
+        .then((response) => response.json())
+        .then((data) => {
+
+            let matchTotales = data.matches;
+
+            let partidosObtenidos = obtenerPartidos(matchTotales);
+            if (partidosObtenidos.length != 0) {
+                let partiditosFiltrados = partidosFiltradosPorVED(partidosObtenidos);
+                pintarPartidosObtenidos(partiditosFiltrados);
+            }
+        })
+}
+
+
 let botonPulsado = function () {
-    let equipo = inputBuscador.value;
-    let partidos = obtenerPartidos(equipo);
-    console.log(partidos);
-    let partidosObtenidos = partidosFiltradosPorVED(partidos);
-    pintarPartidosObtenidos(partidosObtenidos);
+    obtenerInformacion();
+
 };
 
 
-let obtenerPartidos = function (equipoSeleccionado) {
+
+
+let obtenerPartidos = function (todosLosPartidos) {
     partidosTotales.innerHTML = "";
     let partidosDelEquipo = [];
-    if (equipoSeleccionado == undefined || equipoSeleccionado == "") {
-        pintarPartidosObtenidos(matchTotales);
+    if (inputBuscador.value == undefined || inputBuscador.value == "") {
+        pintarPartidosObtenidos(todosLosPartidos);
+        return [];
     }
     else {
-        for (i = 0; i < matchTotales.length; i++) {
-            if (matchTotales[i].homeTeam.name == equipoSeleccionado || matchTotales[i].awayTeam.name == equipoSeleccionado) {
-                partidosDelEquipo.push(matchTotales[i]);
+        for (i = 0; i < todosLosPartidos.length; i++) {
+            if (todosLosPartidos[i].homeTeam.name == inputBuscador.value || todosLosPartidos[i].awayTeam.name == inputBuscador.value) {
+                partidosDelEquipo.push(todosLosPartidos[i]);
 
             }
         }
@@ -38,9 +60,9 @@ let obtenerPartidos = function (equipoSeleccionado) {
 };
 
 let partidosFiltradosPorVED = function (partidosSeleccionados) {
-    if (partidosSeleccionados.length == 0){
+    if (partidosSeleccionados.length == 0) {
         borrarTabla();
-        mensajeError.innerHTML= "No se ha obtenido resultados";
+        mensajeError.innerHTML = "No se ha obtenido resultados";
     }
     let partidosFiltrados = [];
     for (i = 0; i < partidosSeleccionados.length; i++) {
@@ -77,4 +99,4 @@ let borrarTabla = function () {
 
 
 // llAMADO DE FUNCIONES
-obtenerPartidos();
+obtenerInformacion();
